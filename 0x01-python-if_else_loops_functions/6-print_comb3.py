b626f86fca8059de79f6b2f8f7f5b75a5fe4dabb
stevovenom <stephenokoth2002@gmail.com>
2023-11-28 02:00:10 -0800

    The task involves a program that prints all possible different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for numbers in range(10):
     for digits in range(numbers + 1, 10):
-        print(f'{numbers}{digits}', end=", " if (numbers, digits) != (8, 9)
-              else "\n")
+        if numbers == 8 and digits == 9:
+            print(f'{numbers}{digits}')
+        else:
+            print(f'{numbers}{digits}', end=", ")
