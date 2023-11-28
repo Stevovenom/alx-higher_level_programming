874b1243df2a11683897ce9b9900bf1bf55ddfe6
stevovenom <stephenokoth2002@gmail.com>
2023-11-28 01:40:31 -0800

    Update for the 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-first_letter = ord('a')
-for offset in range(6) and  letter != 'q' and letter != 'e':
-    letter = chr(first_letter + offset)
-    print(letter, end="")
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
