d9ce6b393aea261029fdcedf2267a6d3285432a3
stevovenom <stephenokoth2002@gmail.com>
2023-11-27 23:50:47 -0800

    It prints ASCII letters excluding q and e

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+first_letter = ord('a')
+for offset in range(26):
+    letter = chr(first_letter + offset)
+    if letter != 'q' and  letter != 'e':
+        print(letter, end="")
