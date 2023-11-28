e261c14ac74ced4fd31b9440dfb809134c4d24f7
stevovenom <stephenokoth2002@gmail.com>
2023-11-28 02:03:56 -0800

    Update for the 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for numbers in range(10):
     for digits in range(numbers + 1, 10):
         if numbers == 8 and digits == 9:
-            print(f'{numbers}{digits}')
+            print("{}{}".format(numbers, digits))
         else:
-            print(f'{numbers}{digits}', end=", ")
+            print("{}{}".format(numbers, digits), end=", ")
