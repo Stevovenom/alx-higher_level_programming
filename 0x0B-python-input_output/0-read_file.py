37c1490a8b98506c9730927f420397c565166afd
stevovenom <stephenokoth2002@gmail.com>
2024-01-10 13:01:01 -0800

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 
-
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """
+    Reads a text file (UTF-8) and prints its contents to stdout.
+
+    Args:
+    - filename (str): The path to the text file.
+
+    Raises:
+    - FileNotFoundError: If the file specified by filename does not exist.
+    - PermissionError: If the user does not have permission to read the file.
+
+    Returns:
+    - None
+    """
+    try:
+        with open(filename, "r", encoding="UTF-8") as f:
+            print(f.read(), end="")
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+    except PermissionError:
+        print(f"Error: Permission denied for file '{filename}'.")
+
