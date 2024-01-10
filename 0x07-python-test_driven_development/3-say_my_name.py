1cfb042b233d197a3b48ba5b3d52afa0b1e80a5d
stevovenom <stephenokoth2002@gmail.com>
2024-01-10 08:25:08 -0800

    The function prints prints My name is <first name> <last name>

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+    """
+    Prints the full  name consisting of the first name and the last name.
+
+    Args:
+    first_name (str): The first name.
+    last_name (str, optional): last name.
+
+    Returns:
+    None: This function does not return a valu.
+
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
