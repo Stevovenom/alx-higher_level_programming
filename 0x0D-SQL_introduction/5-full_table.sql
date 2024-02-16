be9b448101679dab8736dddf8349ad06718619a7
Stevovenom <stephenokoth2002@gmail.com>
2024-02-16 02:05:52 -0800

    The decription of a database without the use of 'DESCRIBE' or 'EXPLAIN'

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- Describes the named DATABASE
-SELECT column_name, column_type, is_nullable, column_default
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0'
-AND table_name = 'first_table';
+SHOW CREATE TABLE first_table;
