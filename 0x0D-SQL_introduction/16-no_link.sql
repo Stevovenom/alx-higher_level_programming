5480c5fc6c3920b3b73522ad17906b091e165987
Stevovenom <stephenokoth2002@gmail.com>
2024-02-16 11:23:02 -0800

    update

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- lists all records of the table second_table of the database
+-- Don’t list rows without a name value
+SELECT score, name FROM second_table WHERE name != '' AND name IS NOT NULL ORDER BY score DESC;
