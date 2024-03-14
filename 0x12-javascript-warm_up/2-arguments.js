f410c876f307144c9b78429b363d2c69970a7940
Stevovenom <stephenokoth2002@gmail.com>
2024-03-13 23:19:09 -0700

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 // the use of process
 const argCount = process.argv.length - 2;
-if (argCount === 0) {
-	console.log("No argument");
+if (argCount === 0)
+{
+	console.log('No argument');
 }
 else if (argCount === 1) {
-	console.log("Argument found");
+	console.log('Argument found');
 }
 else {
-	console.log("Arguments found");
+	console.log('Arguments found');
 }
