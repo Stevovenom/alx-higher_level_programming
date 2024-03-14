4d50ee9bdc692c33cdc7a04a877aac31249c482d
Stevovenom <stephenokoth2002@gmail.com>
2024-03-13 23:04:16 -0700

    update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // The script is to print 3 lines
 
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
