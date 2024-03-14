b4b1694ef00ff03b3435792e0ed69f740e9b2538
Stevovenom <stephenokoth2002@gmail.com>
2024-03-13 21:13:20 -0700

    The scripts displays 3 liines each which the stated words, but generally it aims to highlighten the knowledge of declaration of more variables using const

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// The script is to print 3 lines
+const Var1 = 'Cis fun';
+const Var2 = 'Python is cool';
+const Var3 = 'JavaScript is amazing';
+console.log(Var1);
+console.log(Var2);
+console.log(Var3);
