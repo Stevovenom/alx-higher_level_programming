45a7df20f4f1f6a8492de63733df17af64e743cd
Stevovenom <stephenokoth2002@gmail.com>
2024-03-14 01:40:44 -0700

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // The script prints x times 'c is fun'
-const text = "C is fun";
+const text = 'C is fun';
 if (isNaN(process.argv[2])) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log(text);
