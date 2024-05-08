35ea2d6d0491eb01ec73c9ff18c39355fed63735
Stevovenom <stephenokoth2002@gmail.com>
2024-05-08 11:41:58 -0700

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
     # make a connection to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd="password", db="hbtn_0e_0_usa")
+                         passwd=argv[2], db=argv[3])
 
     # It gives us the ability to have multiple seperate working environments
     # through the same connection to the database.
