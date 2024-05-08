0ba51a37b9ebd96397480714cf1ff304275e1274
Stevovenom <stephenokoth2002@gmail.com>
2024-05-08 11:13:54 -0700

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-# Script that lists all states from the database hbtn_0e_0_usa
+""" Script that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 from sys import argv
-"""
-This condition ensures that the code block beneath it is only executed,
-if the script is run directly and not imported as a module into another script
-"""
+
+# The code should not be executed when imported
 if __name__ == '__main__':
 
-# creating a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user="root",
-            passwd="password", db="hbtn_0e_0_usa")
-# the cursor function that will be used to execute sql queries
-    cur=db.cursor()
-# executes the query that selects all from states
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd="password", db="hbtn_0e_0_usa")
+
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
+    cur = db.cursor()
     cur.execute("SELECT * FROM states")
-# fetches the rows from the states column
-    rows=cur.fetchall()
-# displays the fetched rows
-    for row in rows:
-        print(row)
-# end the proceses
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
     cur.close()
     db.close()
