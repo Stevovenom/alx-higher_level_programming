7d53ed3687d786510eb62ceecce4f7cafbdd7846
Stevovenom <stephenokoth2002@gmail.com>
2024-05-13 06:45:09 -0700

    Attempt

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,9 @@ if __name__ == '__main__':
             passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute('SELECT * FROM cities ORDER BY cities.id ASC')
+    cur.execut('SELECT cities.id, cities.name, states.name FROM cities\
+            JOIN states ON cities.state_id = states.id\
+            ORDER BY cities.id ASC;')
 
     rows = cur.fetchall()
     for i in rows:
