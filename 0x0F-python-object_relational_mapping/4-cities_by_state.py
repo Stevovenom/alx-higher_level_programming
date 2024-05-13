7dc26b271a40a7c636e804fa35b17a77e0a65357
Stevovenom <stephenokoth2002@gmail.com>
2024-05-13 06:31:21 -0700

    Attempt

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""query that shows all cities and are arrenged in ascending order """
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    search = '{}'.format(argv[4])
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+            passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute('SELECT * FROM cities ORDER BY cities.id ASC;', (search))
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
