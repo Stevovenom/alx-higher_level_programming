af26d4ee8c51253b57c5c0b9a8dc5f9a72dd1333
Stevovenom <stephenokoth2002@gmail.com>
2024-05-13 06:35:54 -0700

    Attempt

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,13 +5,11 @@ from sys import argv
 
 if __name__ == '__main__':
 
-    search = '{}'.format(argv[4])
-
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
             passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute('SELECT * FROM cities ORDER BY cities.id ASC;', (search))
+    cur.execute('SELECT * FROM cities ORDER BY cities.id ASC')
 
     rows = cur.fetchall()
     for i in rows:
