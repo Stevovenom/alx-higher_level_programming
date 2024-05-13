b34b8d1fa8c6d69b6022785c9a1a1dc1703a4bee
Stevovenom <stephenokoth2002@gmail.com>
2024-05-13 04:12:40 -0700

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+imports the sql database
+"""
 import MySQLdb
 import sys
 
+# This line checks if the script is being run as the main program
+# (not imported as a module). if it is, the code below itwill be executed
 if __name__ == '__main__':
     if len(sys.argv) != 5:
         print("Usage: ./script.py <username> <password> <database_name> <name>")
@@ -12,6 +17,7 @@ if __name__ == '__main__':
     database = sys.argv[3]
     name_arg = sys.argv[4]
 
+    # connect to the database
     db = MySQLdb.connect(host="localhost", port=3306, user="root",
                          passwd=password, db="hbtn_0e_0_usa")
 
