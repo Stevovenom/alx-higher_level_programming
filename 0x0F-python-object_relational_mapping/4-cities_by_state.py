e9b9778259cf86686f2a0d9b1ce092db5826a194
Stevovenom <stephenokoth2002@gmail.com>
2024-05-15 04:09:16 -0700

    update to the error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,11 +5,14 @@ if __name__ == '__main__':
     import MySQLdb
     from sys import argv
 
+    # creation of the database connection
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
             passwd=argv[2], db=argv[3])
 
+    # the cursor to be used in te excution of te sql query
     cur = db.cursor()
 
+    # execution of the sql query
     cur.execute('SELECT cities.id, cities.name, states.name FROM cities\
             JOIN states ON cities.state_id = states.id\
             ORDER BY cities.id ASC')
@@ -18,5 +21,6 @@ if __name__ == '__main__':
     for i in rows:
         print(i)
 
+    # close the connection
     cur.close()
     db.close()
