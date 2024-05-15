e579e3f608b08db9964602df11194a0ba8afaf5c
Stevovenom <stephenokoth2002@gmail.com>
2024-05-15 04:06:42 -0700

    update to the error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     import MySQLdb
     from sys import argv
 
-    db = MySQLdb.connect(host="localhost", port=3306, user="root",
-            passwd="password", db="hbtn_0e_4_usa")
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+            passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
 
