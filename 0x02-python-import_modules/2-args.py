d63ef80005ed9ed0798ed174b44387c0a514f988
Stevovenom <stephenokoth2002@gmail.com>
2024-05-29 06:23:28 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,3 +11,32 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sys = sys.argv[1:]
+    arg_count = len(argv)
+
+    if arg_count == 0:
+        print(f"{arg_count} arguments.)
+    elif arg_cout == 1:
+        print(f"{arg_count} argument.)
+    else:
+        print(f"{arg_count} arguments)
+
+    for i, arg in enumerate(argv, start=1):
+        print(f"{i}: {arg}")
+"""
---
+++ 0x02-python-import_modules/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    arg_count = len(argv)
+
+    if arg_count == 0:
+        print(f"{arg_count} arguments.")
+    elif arg_count == 1:
+        print(f"{arg_count} argument:")
+    else:
+        print(f"{arg_count} arguments:")
+
+    for i, arg in enumerate(argv, start=1):
+        print(f"{i}: {arg}😂❤")
