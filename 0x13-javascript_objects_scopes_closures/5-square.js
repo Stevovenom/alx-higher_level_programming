0c87c5f87a4d9440b17cbe5a6a28896211ca4a28
stevovenom <stephenokoth2002@gmail.com>
2024-06-19 13:59:06 -0700

    A class Square that defines a square and inherits from Rectangle of 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,27 +1,7 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-  rotate () {
-    let tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
+/* inheritance in JS */
+const Rectangle = require('./4-rectangle');
+// call the inheritance
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
