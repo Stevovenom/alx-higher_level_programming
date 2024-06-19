622218d4dd1eef4626ecf2db40d8f1b54f454699
stevovenom <stephenokoth2002@gmail.com>
2024-06-19 13:55:22 -0700

    class Rectangle that defines a rectangle in javascript

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+/* create an rectangle class in JS */
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
@@ -6,9 +7,19 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
+  // Method
   print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+    let row = '';
+    let count = 0;
+    // we create the first row depending of width
+    for (; count < this.width; count++) {
+      row = row + 'X';
+    }
+    count = 0;
+    // print the row created height times
+    for (; count < this.height; count++) {
+      console.log(row);
     }
   }
 };
