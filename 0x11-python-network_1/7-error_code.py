3c0a54bf0531edc1a03d6ddbe47632ee7351a691
stevovenom <stephenokoth2002@gmail.com>
2024-07-08 03:58:04 -0700

    To achieve the functionality described in the request, one can use Python's requests library to send HTTP requests and handle HTTP status codes

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays
+the body of the response.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]  # Read URL from command-line argument
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for 4xx and 5xx errors
+
+        # If no exception was raised, print the body of the response
+        print(response.text)
+
+    except requests.exceptions.HTTPError as err:
+        print(f"Error code: {err.response.status_code}")
